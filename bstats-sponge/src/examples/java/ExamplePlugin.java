fc189141f79e03ba13ee1f338c718d4f07495bb2
Angelo6838 <72785429+Angelo6838@users.noreply.github.com>
2021-01-30 10:53:53 +0800

    ExamplePlugin.java
    Example

--- bstats-sponge/src/examples/java/ExamplePlugin.java
+++ bstats-sponge/src/examples/java/ExamplePlugin.java
@@ -4,17 +4,17 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = "exampleplugin", name = "ExamplePlugin", version = "1.0")
-public class ExamplePlugin {
+@Plugin(id = "10177", name = "WorldX", version = "1.0")
+public class WorldX {
 
     private final Metrics2 metrics;
 
     // The metricsFactory parameter gets injected using @Inject :)
     // Check out https://docs.spongepowered.org/master/en/plugin/injection.html if you don't know what @Inject does
     @Inject
-    public ExamplePlugin(Metrics2.Factory metricsFactory) {
+    public WorldX(Metrics2.Factory metricsFactory) {
         // You can find the plugin ids of your plugins on the page https://bstats.org/what-is-my-plugin-id
-        int pluginId = 1234; // <-- Replace with the id of your plugin!
+        int pluginId = 10177; // <-- Replace with the id of your plugin!
         metrics = metricsFactory.make(pluginId);
     }
 
